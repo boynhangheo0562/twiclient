d2252b58f173ecb26f5d85cc72140cac408180d4
boynhangheo0562 <boynhangheo0562@gmail.com>
2025-10-24 01:02:24 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+// Root project-level build.gradle.kts
+
 allprojects {
     repositories {
         google()
@@ -5,20 +7,25 @@ allprojects {
     }
 }
 
+// Đổi buildDirectory ra ngoài root project
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+// Subproject build directory riêng
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Đảm bảo app module được evaluate trước
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Task clean cho toàn bộ project
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
